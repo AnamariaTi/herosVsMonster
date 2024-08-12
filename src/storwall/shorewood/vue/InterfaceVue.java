c838bf14026eef1ba6be10caf9b82964fa72d6a6
AnamariaTi <112396875+AnamariaTi@users.noreply.github.com>
2024-08-12 13:59:54 +0200

    miss a projet

--- src/storwall/shorewood/vue/InterfaceVue.java
+++ src/storwall/shorewood/vue/InterfaceVue.java
@@ -4,10 +4,10 @@ import storwall.shorewood.model.Personnage;
 
 public interface InterfaceVue {
     public void display();
-//    public void initCarte();
-//
-//    public void displayCart();
-//    public void displayMessage(String message);
-//
+    public void initCarte();
+
+    public void displayCart();
+    public void displayMessage(String message);
+
 
 }
